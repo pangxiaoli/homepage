638f50c375c54a80a2cbdc1f3041d195dfddf740
pangxiaoli <qiubit@qiubit.cc>
2023-01-23 19:40:00 +0800

    feat: sider avater

--- config.ts
+++ config.ts
@@ -31,7 +31,7 @@ const config: IConfig = {
 	subTitle: 'subtitle',
 
 	user: {
-		avater: '',
+		avater: 'public/head.jpg',
 		name: 'pangxiaoli',
 	},
 
--- src/components/sider/index.tsx
+++ src/components/sider/index.tsx
@@ -1,6 +1,7 @@
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
+import useConfig from '../../hooks/useConfig';
 import useThem from '../../hooks/useTheme';
 import './index.less';
 
@@ -14,7 +15,9 @@ const Sider = () => {
 	};
 	return (
 		<div className="sider">
-
+			<div className="avater">
+				<img src={useConfig( 'user' ).avater} alt="avater" />
+			</div>
 			<div className="theme-switcher" onClick={handleSwitchTheme}>
 				<FontAwesomeIcon icon={theme === 'light' ? faMoon : faSun} />
 			</div>
