2d6cf158f155684e54a5856e75e11172d2b38503
pangxiaoli <qiubit@qiubit.cc>
2023-01-27 23:37:06 +0800

    chore: 主题配色

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,16 +5,16 @@ import './index.less';
 const Footer = () => {
 	return (
 		<div className="footer">
-			<div className="footer-item">©{useConfig( 'footer' ).year} {useConfig( 'footer' ).copyRight}</div>
+			<div className="footer-item">©{useConfig( 'footer' )?.year} {useConfig( 'footer' )?.copyRight}</div>
 			<div className="footer-item">
 				<a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">
-					{useConfig( 'footer' ).icp}
+					{useConfig( 'footer' )?.icp}
 				</a>
 			</div>
 			<div className="footer-item">
-				<a href={'http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=' + useConfig( 'footer' ).gunan.match( /\d.*/ )[0]} target="_blank" rel="noopener noreferrer">
+				<a href={'http://www.beian.gov.cn/portal/registerSystemInfo?recordcode='} target="_blank" rel="noopener noreferrer">
 					<img src={beianIcon} alt="beian" />
-					{useConfig( 'footer' ).gunan}
+					{useConfig( 'footer' )?.gunan}
 				</a>
 			</div>
 		</div>
